829f3dd24a41cfefb2bdca96e49420d652b557a1
Jan-Marco Reiners <jr@zinklerbrandes.com>
2021-02-16 14:38:41 +0100

    init

---
+++ test/controllers/staticpages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class StaticpagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get staticpages_home_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get staticpages_contact_url
+    assert_response :success
+  end
+end
